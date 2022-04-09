1be302d3df265e0ab4b76887ec5fcb2740fc64f2
Xavier Lin <xavier.li.1332@gmail.com>
2022-04-09 13:44:57 -0400

    Reorginizing the repo

--- app.py
+++ app.py
@@ -1,29 +1,36 @@
 from flask import Flask, render_template, request
-from datetime import datetime
-import re
+from keras.models import load_model
+from keras.preprocessing import image
+from sklearn import metrics
+from werkzeug.utils import secure_filename
+import numpy as np
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='template')
+dic = {0 : 'melanoma', 1 : 'basal cell carcinoma', 2 : 'squamous cell carcinoma'}
 
+model = load_model('model.h5')
+model.make_predict_function()
 
-## EMPTY HTML FILES
-@app.route("/")
-def home():
-    return render_template("index.html", prediction = None)
+def modelPrediction(img_path):
+	img = image.load_img(img_path, target_size=(100,100))
+	img = image.img_to_array(img)/255.0
+	img = img.reshape(1, 100,100,3)
+	predict_x=model.predict(img)
+	p=np.argmax(predict_x,axis=1)
+	return dic[p[0]]
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
-    
+@app.route("/", methods=['GET', 'POST'])
+def main():
+	return render_template("index.html")
 
-##passes an image into our ML Model and 
-@app.route("/submit", methods = ['POST'])
-def submit(name = None):
-    if request.method == 'POST':
-        img = request.files['image name']  ## IMAGE NAME DEPENDS ON NAME INSIDE THE HTML INPUT ELEMENT!!
-        path = "static/predicting" +img.name
-        img.save(path)
-    
-    ##put call to ML model right here (replace null)
-    prediction = None
-    
-    return render_template("index.html", prediction = prediction)
+@app.route("/submit", methods = ['GET', 'POST'])
+def output():
+	if request.method == 'POST':
+		img = request.files['my_image']
+
+		img_path = "static/" + img.filename	
+		img.save(img_path)
+		p = modelPrediction(img_path)
+	return render_template("index.html", prediction = p, img_path = img_path)
+if __name__ =='__main__':
+	app.run(debug = True)
